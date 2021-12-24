96b9d9cd895b9bf19aa6c8f77249f560c510b91a
mikeamorim <migamorim2012@gmail.com>
2021-12-24 13:15:52 +0000

    Navbar with Bootstrap

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,23 +1,15 @@
-<div class="row" style="background: white">
-    <div class="col-md-2">
-        <h1><a href="{{ url('/homepage') }}">
-                <img src="{{ asset('/images/planwiserlogo.png') }}" style="height: 60px; width: 100%"/>
-            </a></h1>
-    </div>
-    <div class="col-md-10">
-        <div class="container" style="top: 20%;">
-            <a class="button" > About us </a>
-            <a class="button" > Support </a>
-            @if (Auth::check())
-                <a class="button" > Projects </a>
-            @endif
-            @if (Auth::check())
-                <a id="username-btn" class="button" href="{{ url('/profile/'.Auth::id()) }}"> {{ Auth::user()->username }} </a>
-                <a id="logout-btn" class="button float-right" href="{{ url('/logout') }}"> Logout </a>
-            @else
-                <a id="signup-btn" class="button" href="{{ url('/register') }}"> Sign-up </a>
-                <a id="login-btn" class="button" href="{{ url('/login') }}"> Login </a>
-        </div>
-    </div>
+<div class="navbar-align">
+    <a href="{{ url('/homepage') }}"><img src="{{ asset('/images/planwiserlogo.png') }}" style="width: 240px;"/></a>
+    <a class="btn btn-outline-success" style="border-style:hidden; margin-top: 5%;"> About us </a>
+    <a class="btn btn-outline-success" style="border-style:hidden; margin-top: 5%;"> Support </a>
+    @if (Auth::check())
+    <a class="btn btn-outline-success" style="border-style:hidden; margin-top: 5%;"> Projects <a>
+@endif
+@if (Auth::check())
+    <a class="btn btn-outline-success" style="border-style:hidden; margin-top: 5%;" href="{{ url('/profile/'.Auth::id()) }} "> {{ Auth::user()->username }} </a>
+    <a class="btn btn-outline-success" style="border-style:hidden; margin-top: 5%;" href="{{ url('/logout') }}"> Logout </a>
+@else
+    <a id="signup-btn" class="btn btn-outline-success" style="border-style:hidden; margin-top: 5%;" href="{{ url('/register') }}"> Sign-up </a>
+    <a id="login-btn" class="btn btn-outline-success" style="border-style:hidden; margin-top: 5%;" href="{{ url('/login') }}"> Login </a>
 @endif
 </div>
