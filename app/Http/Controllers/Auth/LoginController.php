cdb83afb694642ed746d2def339cbc95be33f352
aluisafmarques <aluisamarques2001@gmail.com>
2021-12-29 10:59:08 +0000

    Delete double import

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\ProjectsController;
-
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\ProjectsController;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
