ad28a2b36e331cdca8c72af675366c466a8af606
marga <margarida.mrro@gmail.com>
2022-01-01 23:07:36 +0000

    Restore task assignment

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Tasks;
 use App\Models\Project;
+use App\Models\UserAssigns;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
