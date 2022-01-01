6bd3b54908c0ece1200666f4ec7064675b471f23
marga <margarida.mrro@gmail.com>
2022-01-01 20:45:02 +0000

    Add task assignment and removal

--- app/Http/Controllers/ProjectUsersController.php
+++ app/Http/Controllers/ProjectUsersController.php
@@ -28,14 +28,22 @@ class ProjectUsersController extends Controller
         $this->middleware('auth');
     }
 
-    public function showProjectUsers($project_id)
-    {
-        $notifications = NotificationsController::getNotifications(Auth::id());
+    static public function getProjectUsers($project_id){
         $myusers = DB::table('users')
                         ->join('projectusers', 'users.id', '=', 'projectusers.user_id')
                         ->where('projectusers.project_id', $project_id)
-                        ->get(['username','email','user_role']);
+                        ->get(['user_id', 'username','email','user_role']);
         $myusers = json_decode($myusers,true);
+
+        return $myusers;
+    }
+
+    public function showProjectUsers($project_id)
+    {
+        $notifications = NotificationsController::getNotifications(Auth::id());
+        
+        $myusers = $this->getProjectUsers($project_id);
+
         return view('pages.projectUsers',['project_users' => $myusers,'project' => Project::find($project_id), 'notifications' => $notifications]);
     }
 }
