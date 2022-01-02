f728b0fee79f325db3c8b0ce4ea6611abce61911
NanD0245 <fernandoluisrego@gmail.com>
2022-01-02 18:39:22 +0000

    add admin policy

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,17 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Card;
+use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class AdminController extends Controller
 {
 
     public function show()
     {
+        Gate::authorize('admin',User::class);
         $public_projects = ProjectsController::getPublicProjects(6);
         $users = UsersController::getUsers();
         return view('pages.admin.home', ['public_projects'=>$public_projects, 'users'=>$users]);
@@ -21,6 +24,7 @@ class AdminController extends Controller
 
     public function showReports()
     {
+        Gate::authorize('admin',User::class);
         $public_projects = ReportsController::getReports();
         return view('pages.admin.reports', ['public_projects'=>$public_projects]);
 
@@ -28,40 +32,47 @@ class AdminController extends Controller
 
     public function showUsersManagement()
     {
+        Gate::authorize('admin',User::class);
         $users = UsersController::getUsers();
         return view('pages.admin.manageUsers',['users' => $users]);
     }
 
     public function showProjects()
     {
+        Gate::authorize('admin',User::class);
         $public_projects = ProjectsController::getPublicProjects(6);
         return view('pages.admin.projects', ['public_projects'=>$public_projects]);
     }
 
     public function showProfile(int $id)
     {
+        Gate::authorize('admin',User::class);
         $user = UsersController::getUser($id);
         return view('pages.admin.profile',['user' => $user]);
     }
 
     public function showUsers()
     {
+        Gate::authorize('admin',User::class);
         $users = UsersController::getUsers();
         return view('pages.admin.manageUsers',['users' => $users]);
     }
 
     public function searchUsers(Request $request){
+        Gate::authorize('admin',User::class);
         $users = UsersController::getUsersSearch($request);
-        return view('pages.admin.manageUsers', ['users'=>$users]);  
+        return view('pages.admin.manageUsers', ['users'=>$users]);
     }
 
     public function showUsersForm()
     {
+        Gate::authorize('admin',User::class);
         return view('pages.admin.createUser');
     }
 
     public function createUser(Request $request)
     {
+        Gate::authorize('admin',User::class);
         $user = UsersController::createUser($request);
         return redirect()->action([self::class,'showProfile'], ['id'=> $user->id]);
 
