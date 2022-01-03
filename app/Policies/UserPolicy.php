f05b09adf796ffbab0876ae94af9fcda3a4ada5c
NanD0245 <fernandoluisrego@gmail.com>
2022-01-03 11:41:57 +0000

    fix policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -12,12 +12,12 @@ class UserPolicy
 
     public function show(User $auth, User $user)
     {
-        return $auth->id == $user->id || $user->is_admin;
+        return $auth->id == $user->id || $auth->is_admin;
     }
 
     public function update(User $auth, User $user)
     {
-        return $user->id == $auth->id || $user->is_admin;
+        return $user->id == $auth->id || $auth->is_admin;
     }
 
     public function admin(User $user) {
