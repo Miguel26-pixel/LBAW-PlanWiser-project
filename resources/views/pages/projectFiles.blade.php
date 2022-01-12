d091b9c0c95f20787c87c4fe671073dc068903e7
marga <margarida.mrro@gmail.com>
2022-01-12 12:35:45 +0000

    Improve project files design

--- resources/views/pages/projectFiles.blade.php
+++ resources/views/pages/projectFiles.blade.php
@@ -19,75 +19,57 @@
             <div class="mt-4 container align-items-center">
                 <h3>Files</h3>
             </div>
-            <div class="row m-0">
-                <div class="col-md-8">
-                    <div class="container">
-                        <table class="table table-bordered  my-3">
+            <div class="col-md-12 px-4 my-4">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-between align-items-center">
+                        <?php if ($user_role != 'GUEST') {?>
+                            <form action="/project/{{$project->id}}/files/upload-files" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="input-group my-3">
+                                    <input name="input_files[]" class="form-control" type="file" id="formFile" multiple required>
+                                    <button class="btn btn-outline-success" type="submit" id="formFile">Upload</button>
+                                </div>
+                            </form>
+                        <?php } ?>
+                        <a href="/project/{{$project->id}}/files/downloadZIP" class="btn btn-outline-success" style="border-style:hidden;"><i class="icon-folder-alt"></i> {!! "&nbsp;" !!} Download ZIP</a>
+                    </div>
+                    <div class="card-header d-flex align-items-center">
+                       
+                        <div>
+                            <span class="text-center text-danger"><i class="icon-shield"></i> Warning:{!! "&nbsp;" !!}</span>
+                            Uploading files with a filename that already exists will replace the existing file
+                        </div>
+                    </div>
+
+                <div id="publicCardBody" class="card-body">
+                        <table class="table table-bordered">
                             <thead class="table-success" >
-                            <tr>
-                                <th scope="col" style="width: 5%">Type</th>
-                                <th scope="col">File Name</th>
-                                <th scope="col" class="text-center" style="width: 15%">Upload Date</th>
-                                <?php if ($user_role != 'GUEST') {?>
-                                <th scope="col" class="text-center" style="width: 10%">Delete</th>
-                                <?php } ?>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <?php
-                            $count=0;
-                            foreach ($files as $file) {
-                                echo '<tr>';
-                                echo '<td class="text-center text-primary"><i class="icon-doc"></i></td>';
-                                echo '<td><a href="/project/'.$project['id'].'/files/'.$file->id.'/download" class="text-primary" style="text-decoration: none;">'.$file['name'].'</a></td>';
-                                echo '<td class="text-center">'.$file['updated_at'].'</td>';
-                                if ($user_role != 'GUEST') {
-                                echo '<td class="text-center"><a class="btn btn-outline-danger" href="/project/'.$project['id'].'/files/'.$file->id.'/delete"><i class="icon-trash"></i></td>';
+                                <tr>
+                                    <th scope="col" style="width: 5%">Type</th>
+                                    <th scope="col">File Name</th>
+                                    <th scope="col" class="text-center" style="width: 15%">Upload Date</th>
+                                    <?php if ($user_role != 'GUEST') {?>
+                                    <th scope="col" class="text-center" style="width: 10%">Delete</th>
+                                    <?php } ?>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                $count=0;
+                                foreach ($files as $file) {
+                                    echo '<tr>';
+                                    echo '<td class="text-center text-primary"><i class="icon-doc"></i></td>';
+                                    echo '<td><a href="/project/'.$project['id'].'/files/'.$file->id.'/download" class="text-primary" style="text-decoration: none;">'.$file['name'].'</a></td>';
+                                    echo '<td class="text-center">'.$file['updated_at'].'</td>';
+                                    if ($user_role != 'GUEST') {
+                                    echo '<td class="text-center"><a class="btn btn-outline-danger" href="/project/'.$project['id'].'/files/'.$file->id.'/delete"><i class="icon-trash"></i></td>';
+                                    }
+                                    echo '</tr>';
+                                    $count++;
                                 }
-                                echo '</tr>';
-                                $count++;
-                            }
-                            ?>
+                                ?>
                             </tbody>
                         </table>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="container">
-                        <h3 class="text-center">Options</h3>
-                        <div class="card">
-                            <div class="card-body">
-                                <?php if ($user_role != 'GUEST') {?>
-                                <h4 class="text-center">Upload File(s)</h4>
-                                <form action="/project/{{$project->id}}/files/upload-files" method="POST" enctype="multipart/form-data">
-                                    @csrf
-                                    <div class="input-group my-3">
-                                        <input name="input_files[]" class="form-control" type="file" id="formFile" multiple required>
-                                        <button class="btn btn-outline-success" type="submit" id="formFile">Upload</button>
-                                    </div>
-                                </form>
-                                <br>
-                                <?php } ?>
-                                <!--h4 class="text-center">Upload Folder</h4>
-                                <form action="/project/{{$project->id}}/files/upload-folder" method="POST" enctype="multipart/form-data">
-                                    @csrf
-                                    <div class="input-group my-3">
-                                        <input name="input_folder[]" class="form-control" type="file" id="formFolder" webkitdirectory>
-                                        <input name="dir_name" type="text" id="dir_name" class="d-none">
-                                        <button class="btn btn-outline-success" type="submit" id="formFolder">Upload</button>
-                                    </div>
-                                </form>
-                                <br-->
-                                <h4 class="text-center">Download ZIP</h4>
-                                <div class="text-center my-3">
-                                    <a href="/project/{{$project->id}}/files/downloadZIP" class="btn btn-outline-success"><i class="icon-folder-alt"></i></a>
-                                </div>
-                            </div>
-                            <div class="card-footer">
-                                <span class="text-center text-danger"><i class="icon-shield"></i> Warning:</span>
-                                Uploading files with a filename that already exists will replace the existing file
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
