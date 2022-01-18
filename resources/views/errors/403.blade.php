84d3daedcce732d70b3a822989bfae57f1cc2e85
mikeamorim <migamorim2012@gmail.com>
2022-01-18 21:43:20 +0000

    Update 403 and 404 pages

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,19 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.errors')
 
+@section('errors')
 <div class="error-body">
+    <div class="error-container">
+        <div class="error-neon">403</div> 
+    </div>
     <div class="error-message">You are not authorized.</div>
     <div class="error-message2">You tried to access a page you did not have prior authorization for.</div>
-    <div class="error-container">
-        <div class="error-neon">403</div>
-        <div class="error-door-frame">
-            <div class="error-door">
-                <div class="error-rectangle"></div>
-            </div>
-                <div class="error-handle"></div>
-                <div class="error-window">
-                    <div class="error-eye"></div>
-                </div>
-                <div class="error-leaf"></div> 
-        </div>  
-    </div>
 </div>
+@endsection
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,19 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.errors')
 
+@section('errors')
 <div class="error-body">
     <div class="error-message">Page not found.</div>
     <div class="error-message2">You tried to access a page that doesn't exist.</div>
     <div class="error-container">
-        <div class="error-neon">404</div>
-        <div class="error-door-frame">
-            <div class="error-door">
-                <div class="error-rectangle"></div>
-            </div>
-                <div class="error-handle"></div>
-                <div class="error-window">
-                    <div class="error-eye"></div>
-                </div>
-                <div class="error-leaf"></div> 
-        </div>  
+        <div class="error-neon">404</div> 
     </div>
 </div>
+@endsection
