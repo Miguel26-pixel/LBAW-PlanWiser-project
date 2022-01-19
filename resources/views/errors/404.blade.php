e6608e156f6db680bfb77cc509c9871eec8714ab
mikeamorim <migamorim2012@gmail.com>
2022-01-19 12:29:14 +0000

    Fix errors pages responsivity

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,10 +2,16 @@
 
 @section('errors')
 <div class="error-body">
-    <div class="error-message">Page not found.</div>
-    <div class="error-message2">You tried to access a page that doesn't exist.</div>
-    <div class="error-container">
-        <div class="error-neon">404</div> 
+    <div class="error-container row m-0" style="padding-top: 15%;">
+        <div class="error-number col-md-6">
+            <div class="error-neon">404</div> 
+        </div>
+        <div class="error-start-flex col-md-6">
+            <div class="error-meaning">
+                <div class="error-message">Page not found.</div>
+                <div class="error-message2">You tried to access a page that doesn't exist.</div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
